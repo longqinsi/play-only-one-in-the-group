8d54c6ce93f67784a4c02cd788d3edcadfb65e49
longqinsi <longqinsi@hotmail.com>
2022-09-30 22:37:29 +0800

    remove the cancel button from the recording dialog

--- only_one_qt5.py
+++ only_one_qt5.py
@@ -2,8 +2,12 @@ from typing import Callable
 
 import aqt
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+from aqt import qconnect
 from aqt.sound import RecordDialog
+from aqt.utils import restoreGeom
 
 
 class CustomRecordDialog(RecordDialog):
@@ -24,3 +28,26 @@ class CustomRecordDialog(RecordDialog):
             event.accept()
         else:
             super().keyPressEvent(event)
+
+    def showEvent(self, event: QShowEvent):
+        super().showEvent(event)
+
+    def _setup_dialog(self) -> None:
+        self.setWindowTitle("Anki")
+        icon = QLabel()
+        icon.setPixmap(QPixmap("icons:media-record.png"))
+        self.label = QLabel("...")
+        hbox = QHBoxLayout()
+        hbox.addWidget(icon)
+        hbox.addWidget(self.label)
+        v = QVBoxLayout()
+        v.addLayout(hbox)
+        b = QDialogButtonBox(QDialogButtonBox.StandardButton.Save)  # type: ignore
+        v.addWidget(b)
+        self.setLayout(v)
+        save_button = b.button(QDialogButtonBox.StandardButton.Save)
+        save_button.setDefault(True)
+        save_button.setAutoDefault(True)
+        qconnect(save_button.clicked, self.accept)
+        restoreGeom(self, "audioRecorder2")
+        self.show()
